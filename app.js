c8bd1ea34853ca5d029e93c1f7920dcbdd68aa04
Kohrak <kohrakgk0h@gmail.com>
2018-03-18 12:22:23 +0000

    add search functionality and search page

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 var key = "http://www.omdbapi.com/?i=tt3896198&apikey=a0a29a51"
+
+app.get("/", function(req, res){
+    console.log("acces home")
+    res.render("search");
+})
+
 app.get("/results", function(req, res){
     console.log("access results");
-    request(key + "&s=california", function(error, response, body){
+    var keywords = req.query.searchq;
+    var url = key + "&s=" + keywords;
+    request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
             res.render("results", {data: data});
